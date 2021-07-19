9f4eea51b39499622b88c1e8f0b2685d2fe2eda3
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-07-19 16:07:33 +0200

    feat: open and close modal

--- Deliciousness/src/components/Layout/Header.js
+++ Deliciousness/src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
 		<Fragment>
 			<header className={classes.header}>
 				<h1>DeliciousNess</h1>
-				<HeaderCartButton />
+				<HeaderCartButton onClick={props.onShowCart} />
 			</header>
 			<div className={classes["main-image"]}>
 				<img src={cibo} alt="what deliscious good stuff" />
