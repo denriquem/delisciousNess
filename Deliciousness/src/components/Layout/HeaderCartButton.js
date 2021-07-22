daa51286f556613ebab6a415a72f702481202162
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-07-22 10:52:46 +0200

    Feat: context used for button

--- Deliciousness/src/components/Layout/HeaderCartButton.js
+++ Deliciousness/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/Cart-Context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+	const CartCtx = useContext(CartContext);
+
+	const numberOfCartItems = CartCtx.items.reduce((currNumber, item) => {
+		return currNumber + item.amount;
+	}, 0);
+
 	return (
 		<button className={classes.button} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
