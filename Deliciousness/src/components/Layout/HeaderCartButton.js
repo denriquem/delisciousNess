36c16971ddf68eb31da277023ea65a390e9414dc
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-07-29 10:53:45 +0100

    bumping button

--- Deliciousness/src/components/Layout/HeaderCartButton.js
+++ Deliciousness/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/Cart-Context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+	const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
 	const CartCtx = useContext(CartContext);
 
 	const numberOfCartItems = CartCtx.items.reduce((currNumber, item) => {
 		return currNumber + item.amount;
 	}, 0);
 
+	const btnClasses = `${classes.button} ${
+		buttonIsHighlighted ? classes.bump : ""
+	}`;
+
+	const { items } = CartCtx;
+	useEffect(() => {
+		if (CartCtx.items.length === 0) {
+			return;
+		}
+		setButtonIsHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setButtonIsHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
