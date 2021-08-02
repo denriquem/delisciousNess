246132e84f7ec0cc2b6a9a32a17b8301c004cb2e
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-08-02 20:47:31 +0100

    error handling

--- Deliciousness/src/components/Meals/AvailableMeals.js
+++ Deliciousness/src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import Card from "../UI/Card";
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [httpError, setHttpError] = useState();
 
 	useEffect(() => {
 		const fetchMeals = async () => {
@@ -13,6 +14,11 @@ const AvailableMeals = () => {
 			const response = await fetch(
 				"https://dmgproject1-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
 			).then();
+
+			if (!response.ok) {
+				throw new Error("Something went wrong!");
+			}
+
 			const responseData = await response.json();
 
 			const loadedMeals = [];
@@ -28,13 +34,25 @@ const AvailableMeals = () => {
 			setMeals(loadedMeals);
 			setIsLoading(false);
 		};
-		fetchMeals();
+
+		fetchMeals().catch((error) => {
+			setIsLoading(false);
+			setHttpError(error.message);
+		});
 	}, []);
 
 	if (isLoading) {
 		return (
 			<section className={classes.MealsLoading}>
 				<p>...Loading</p>
+			</section>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<section className={classes.MealsError}>
+				<p>{httpError}</p>
 			</section>
 		);
 	}
